96a7d1f8344aaff541996f5f23cf23f82d13e93a
avarvit <avarvit@a2c71350-5c91-11de-bf18-715dc9eb9811>
2011-01-26 05:33:00 +0000

    Joint copyright and acknowledgement added for funded parts of the code

--- trunk/LinuxDahdiDriver/oufxs/oufxs.c
+++ trunk/LinuxDahdiDriver/oufxs/oufxs.c
@@ -1,6 +1,8 @@
 /*
  *  oufxs.c: DAHDI-compatible Linux kernel driver for the Open USB FXS board
  *  Copyright (C) 2010  Angelos Varvitsiotis
+ *  Parts of code (dahdi channel # persistence)
+ *    Copyright (C) 2010  Angelos Varvitsiotis & Rockbochs, Inc.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -14,6 +16,10 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Acknowledgement:
+ *  The author wishes to thank Rockbochs, Inc., for their support and
+ *  for partially funding the development of this driver
  *
  */
 
